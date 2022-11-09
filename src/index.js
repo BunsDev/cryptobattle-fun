ff02afb31fb9732e489831942cf9641362511dc0
Ivan Molto <ivanmolto@gmail.com>
2022-11-09 12:38:00 +0100

    chore: Add routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Footer from "./components/Footer";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Loading from "./components/Loading";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const Results = React.lazy(() => import("./components/Results"));
+const Home = React.lazy(() => import("./components/Home"));
+const Battle = React.lazy(() => import("./components/Battle"));
+const NoMatch = React.lazy(() => import("./components/NoMatch"));
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="flex-col justify-between">
+        <Nav />
+        <React.Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/battle" element={<Battle />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </React.Suspense>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
