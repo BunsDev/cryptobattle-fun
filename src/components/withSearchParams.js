a50dc00bba4cdbc6e39ef52f5985c6d29e2036cf
Ivan Molto <ivanmolto@gmail.com>
2022-11-09 12:54:30 +0100

    chore: Search with params

---
+++ src/components/withSearchParams.js
@@ -0,0 +1,10 @@
+import { useSearchParams } from "react-router-dom";
+
+const withSearchParams = (Component) => {
+  return function ComponentWithSearchParams(props) {
+    const [searchParams] = useSearchParams();
+    return <Component {...props} router={{ searchParams }} />;
+  };
+};
+
+export default withSearchParams;
