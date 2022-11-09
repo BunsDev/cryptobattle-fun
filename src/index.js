51e259775033aa3d36cd636726023eaf8925757e
Ivan Molto <ivanmolto@gmail.com>
2022-11-09 23:26:51 +0100

    chore: Add Governance and Donations routes

--- src/index.js
+++ src/index.js
@@ -5,27 +5,43 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Loading from "./components/Loading";
+import Donation from "./components/Donation";
+import Donations from "./components/Donations";
+import Governance from "./components/Governance";
+import Governances from "./components/Governances";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const Results = React.lazy(() => import("./components/Results"));
 const Home = React.lazy(() => import("./components/Home"));
 const Battle = React.lazy(() => import("./components/Battle"));
 const NoMatch = React.lazy(() => import("./components/NoMatch"));
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="flex-col justify-between">
-        <Nav />
-        <React.Suspense fallback={<Loading />}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/battle" element={<Battle />} />
-            <Route path="/results" element={<Results />} />
-            <Route path="*" element={<NoMatch />} />
-          </Routes>
-        </React.Suspense>
-        <Footer />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="flex-col justify-between">
+          <Nav />
+          <React.Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/battle" element={<Battle />} />
+              <Route path="/results" element={<Results />} />
+              <Route path="/donations" element={<Donations />}>
+                <Route path=":donationId" element={<Donation />} />
+              </Route>
+              <Route path="/governances" element={<Governances />}>
+                <Route path=":governanceId" element={<Governance />} />
+              </Route>
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </React.Suspense>
+          <Footer />
+        </div>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
